daebee8d2b3cf940a70874b6a0be9f591ce18ac5
vssukharev <sugharevmail@gmail.com>
2025-03-23 10:52:40 +0200

    Minor adjustments

--- examples/headers/dummy.h
+++ examples/headers/dummy.h
@@ -1,30 +1,11 @@
 
 #include <stdint.h>
 
-/*
-struct Hello
-{
-  ulong transform;
-  bitstruct : uint
-  {
-    uint instanceCustomIndex : 0..20;
-    ushort mask : 21..23;
-  }
-  char a;
-  bitstruct : ulong
-  {
-    char instanceShaderBindingTableRecordOffset : 0..6;
-    uint flags : 7..33;
-  }
-  ulong accelerationStructureReference;
-}
-*/
+int f(uint32_t a, uint32_t b);
 
-typedef struct Hello {
-    uint64_t transform;
-    uint32_t instanceCustomIndex:28;
-    uint16_t mask:4;
-    uint8_t a;
-    uint64_t accelerationStructureReference;
-} Hello;
+#ifdef TEST_COND
+
+int g(uint32_t a, uint32_t b);
+
+#endif
 
