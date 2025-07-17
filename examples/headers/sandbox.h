d3a563775927a4790520638065d2abc5449c34cb
vssukharev <sugharevmail@gmail.com>
2025-07-17 23:52:15 +0300

    Made complete support for inlined records' declarations

--- examples/headers/sandbox.h
+++ examples/headers/sandbox.h
@@ -1,11 +1,19 @@
 
 
-void foo();
-
-enum {
-  ONE, 
-  TWO, 
-  THREE
+struct Hello {
+  struct Dsa { int a; } dsa;
+  union { double b; } sda;
+  enum Asd {
+    FOUR,
+    FIVE,
+    SIX
+  } _kind2;
+  enum {
+    ONE, 
+    TWO, 
+    THREE
+  } _kind1;
+  struct UnknownT* ut;
 };
 
 
