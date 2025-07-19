bf1ad7a7e36de7dd47187de6cd99da1930102446
vssukharev <sugharevmail@gmail.com>
2025-07-19 17:31:53 +0300

    Updated flake.lock; changed types caching method

--- examples/headers/sandbox.h
+++ examples/headers/sandbox.h
@@ -1,18 +1,25 @@
 
+# define LLVM_ATTRIBUTE_C_DEPRECATED(decl, message) \
+  decl __attribute__((deprecated))
 
-struct Hello {
-  struct Dsa { int a; } dsa;
-  enum {
-    ONE, 
-    TWO, 
-    THREE
-  }* _kind1;
-  enum Ee {
-    ONE1, 
-    TWO1, 
-    THREE1
-  }* _kind2;
-  struct UnknownT* ut;
-};
+LLVM_ATTRIBUTE_C_DEPRECATED(
+  LLVMValueRef LLVMBuildNUWNeg(LLVMBuilderRef B, LLVMValueRef V, const char *Name)
+  , "Use LLVMBuildNeg + LLVMSetNUW instead.");
+
+
+// struct Hello {
+//   struct Dsa { int a; } dsa;
+//   enum {
+//     ONE, 
+//     TWO, 
+//     THREE
+//   }* _kind1;
+//   enum Ee {
+//     ONE1, 
+//     TWO1, 
+//     THREE1
+//   }* _kind2;
+//   struct UnknownT* ut;
+// };
 
 
