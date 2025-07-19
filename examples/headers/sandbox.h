0a1351faf95658b455a5522d38699d36c604d9c5
vssukharev <sugharevmail@gmail.com>
2025-07-19 18:54:49 +0300

    Fixed a bug for typedefs translation

--- examples/headers/sandbox.h
+++ examples/headers/sandbox.h
@@ -7,18 +7,16 @@
 //   , "Use LLVMBuildNeg + LLVMSetNUW instead.");
 
 struct Hello {
-  struct { int a; }* dsa;
   enum {
     ONE, 
     TWO, 
     THREE
   }* _kind1;
-  enum Ee {
-    ONE1, 
-    TWO1, 
-    THREE1
-  }* _kind2;
-  struct UnknownT* ut;
 };
+
+typedef int Flags;
+typedef Flags SomeFlags;
+
+void f(SomeFlags fs);
 
 
