2e8bb9ccfa94d6efc4e3674c442cce51b880a47c
vssukharev <sugharevmail@gmail.com>
2025-07-20 18:46:49 +0300

    Improved camel case string translation

--- examples/headers/sandbox.h
+++ examples/headers/sandbox.h
@@ -1,29 +1,4 @@
 
-#ifndef LLVM_C_EXTERNC_H
-#define LLVM_C_EXTERNC_H
-
-#ifdef __clang__
-#define LLVM_C_STRICT_PROTOTYPES_BEGIN                                         \
-  _Pragma("clang diagnostic push")                                             \
-      _Pragma("clang diagnostic error \"-Wstrict-prototypes\"")
-#define LLVM_C_STRICT_PROTOTYPES_END _Pragma("clang diagnostic pop")
-#else
-#define LLVM_C_STRICT_PROTOTYPES_BEGIN
-#define LLVM_C_STRICT_PROTOTYPES_END
-#endif
-
-#ifdef __cplusplus
-#define LLVM_C_EXTERN_C_BEGIN                                                  \
-  extern "C" {                                                                 \
-  LLVM_C_STRICT_PROTOTYPES_BEGIN
-#define LLVM_C_EXTERN_C_END                                                    \
-  LLVM_C_STRICT_PROTOTYPES_END                                                 \
-  }
-#else
-#define LLVM_C_EXTERN_C_BEGIN LLVM_C_STRICT_PROTOTYPES_BEGIN
-#define LLVM_C_EXTERN_C_END LLVM_C_STRICT_PROTOTYPES_END
-#endif
-
-#endif
-
+int LLVMIsACmpInst();
+int LLVMBuildFPToUI();
 
