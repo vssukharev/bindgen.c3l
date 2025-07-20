e868f85af32770965b0e1e43d3d5fa3f85ed16c4
vssukharev <sugharevmail@gmail.com>
2025-07-20 17:58:51 +0300

    Improved translation formatting for unnamed parameters

--- examples/headers/sandbox.h
+++ examples/headers/sandbox.h
@@ -1,4 +1,29 @@
 
-int LLVMBuildNUWNeg(float b, double v, const char *name) __attribute__((deprecated));
+#ifndef LLVM_C_EXTERNC_H
+#define LLVM_C_EXTERNC_H
+
+#ifdef __clang__
+#define LLVM_C_STRICT_PROTOTYPES_BEGIN                                         \
+  _Pragma("clang diagnostic push")                                             \
+      _Pragma("clang diagnostic error \"-Wstrict-prototypes\"")
+#define LLVM_C_STRICT_PROTOTYPES_END _Pragma("clang diagnostic pop")
+#else
+#define LLVM_C_STRICT_PROTOTYPES_BEGIN
+#define LLVM_C_STRICT_PROTOTYPES_END
+#endif
+
+#ifdef __cplusplus
+#define LLVM_C_EXTERN_C_BEGIN                                                  \
+  extern "C" {                                                                 \
+  LLVM_C_STRICT_PROTOTYPES_BEGIN
+#define LLVM_C_EXTERN_C_END                                                    \
+  LLVM_C_STRICT_PROTOTYPES_END                                                 \
+  }
+#else
+#define LLVM_C_EXTERN_C_BEGIN LLVM_C_STRICT_PROTOTYPES_BEGIN
+#define LLVM_C_EXTERN_C_END LLVM_C_STRICT_PROTOTYPES_END
+#endif
+
+#endif
 
 
