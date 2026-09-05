76a9c4c81ee74881545ff6533045a3975a0159b8
vssukharev <sugharevmail@gmail.com>
2026-09-06 00:16:36 +0300

    Clang headers relocated

---
+++ examples/headers/clang-c/clang.h
@@ -0,0 +1,15 @@
+#ifndef CLANG_INCLUDE
+#define CLANG_INCLUDE
+
+#include "clang-c/Index.h"
+#include "clang-c/CXDiagnostic.h"
+#include "clang-c/CXCompilationDatabase.h"
+#include "clang-c/BuildSystem.h"
+#include "clang-c/CXFile.h"
+#include "clang-c/CXSourceLocation.h"
+#include "clang-c/Documentation.h"
+#include "clang-c/FatalErrorHandler.h"
+#include "clang-c/Platform.h"
+#include "clang-c/Rewrite.h"
+
+#endif
